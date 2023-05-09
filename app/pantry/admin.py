b6b290c75a18501603e2cc42f5cd477c634c6a11
Tann1 <gadkaritanishq@gmail.com>
2023-05-09 00:38:54 +0000

    set up for recipes is there now

--- app/pantry/admin.py
+++ app/pantry/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pantry, Ingredient
+from .models import Pantry, Ingredient,Recipe
 
 admin.site.register(Pantry)
 admin.site.register(Ingredient)
+admin.site.register(Recipe)
--- app/pantry/models.py
+++ app/pantry/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-# Create your models here.
-#dummy user (not actual)
+
+
+"""
+To apply any changes in here:
+ 1. python manage.py makemigrations
+ 2. python manage.py migrate 
+"""
 class User(models.Model):
     username   = models.CharField(max_length=100)
     email      = models.CharField(max_length=200)
@@ -12,6 +17,7 @@ class Ingredient(models.Model):
     name        = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     category    = models.CharField(max_length=50)
+    units       = models.CharField(max_length=50)
 
     def __str__(self):
         return f'''\
@@ -22,8 +28,19 @@ category: {self.category}'''
 class Pantry(models.Model):
     user_id       = models.ForeignKey(User, on_delete=models.CASCADE)
     ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
+    quantity      = models.IntegerField(default=0)
     
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class RecipeIngredients(models.Model):
+    recipe     = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingreidnet = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity   = models.CharField(max_length=50)
 
 
 # TODO proper implementation of user model 
