8c59f959182c7260d1ddde77377886129c10275a
Tann1 <gadkaritanishq@gmail.com>
2023-05-09 19:15:40 +0000

    able to register user

--- app/user_auth/urls.py
+++ app/user_auth/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.login, name="login"),
+    #path("", views.login, name="login"),
     path("register", views.register, name="register"), 
 ]
